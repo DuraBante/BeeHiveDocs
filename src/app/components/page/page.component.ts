2e50704638ceaa1cb99675118b6f7d96be3e00ad
DuraBante <durabante@DuraBantes-Mac-mini.local>
2019-07-29 11:52:14 -0400

    update

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+// Comment
   ngOnInit() {
   }
+gotoCatalog(){
+  this.router.navigate(["catalog"]);
+}
+gotoCreateProcedure(){
+  this.router.navigate(["create-a-procedure"]);
+}
+gotoCreateTemplate(){
+  this.router.navigate(["create-a-template"]);
+}
+gotoCreateItem(){
+  this.router.navigate(["create-and-manage-items"]);
+}
+gotoCreateHive(){
+  this.router.navigate(["creating-a-hive"]);
+}
+gotoDashboard(){
+  this.router.navigate(["dashboard"]);
+}
+gotoData(){
+  this.router.navigate(["data"]);
+}
+gotoDefinition(){
+  this.router.navigate(["definitions"]);
+}
+gotoEditHive(){
+  this.router.navigate(["edit-the-hive"]);
+}
+gotoKPI(){
+  this.router.navigate(["kpi"]);
+}
+gotoLoggingOn(){
+  this.router.navigate(["logging-on"]);
+}
+gotoLoggingOut(){
+  this.router.navigate(["logging-out"]);
+}
+gotoProcedure(){
+  this.router.navigate(["procedure"]);
+}
+gotoRemoveProcedure(){
+  this.router.navigate(["removal-an-incorrectly-initiated-procedure"]);
+}
+gotoSelectHive(){
+  this.router.navigate(["select-a-hive"]);
+}
+gotoSheets(){
+  this.router.navigate(["sheets"]);
+}
 
 }
